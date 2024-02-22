05eb2a74d9a3d8e592288187dde41c49de27d7f9
komotamu <komo@tamu.edu>
2024-02-22 01:09:01 -0500

    add movies
    Signed-off-by: komotamu <komo@tamu.edu>

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Lift', :rating => 'PG-13',
+    :release_date => '10-Jan-2024'},
+  {:title => 'Wish', :rating => 'PG',
+    :release_date => '22-Nov-2023'},
+  {:title => 'Dune', :rating => 'PG-13',
+    :release_date => '03-Jan-2024'}
 ]
 
 more_movies.each do |movie|
